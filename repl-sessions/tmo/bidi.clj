5b551baa51105cde08f53c2840e28ff70294f21e
Thomas Moerman <thomas.moerman@gmail.com>
2015-02-16 23:18:25 +0100

    repl blah

--- repl-sessions/tmo/bidi.clj
+++ repl-sessions/tmo/bidi.clj
@@ -2,12 +2,8 @@
 (use 'bidi.bidi)
 
 (def smi-1 "C[C@@H]1C[C@H](C)C[C@@H](C)C1")
-
-(defn url-encode [s] (java.net.URLEncoder/encode s))
-
-(def url-encoded
-  "A regex matching on url encoded strings."
-  #".*%[0-9a-fA-F]+|\.\.")
+(def smi-2 "CCc(c1)ccc2[n+]1ccc3c2Nc4c3cccc4")
+(def smi-3 "CN1CCC[C@H]1c2cccnc2")
 
 (def all-routes ["/" {"index.html" :index
                       ["molecule/" [url-encoded :smi]] :molecule}])
@@ -15,3 +11,14 @@
 (str "/molecule/" (url-encode smi-1))
 
 (match-route all-routes (str "/molecule/" (url-encode smi-1)))
+(match-route all-routes (str "/molecule/" (url-encode smi-2)))
+
+
+(url-encode smi-1)
+
+(url-encode smi-2)
+
+
+(defn gen-url
+  [smi]
+  (str "http://localhost:8000/molecule/" (url-encode smi)))
