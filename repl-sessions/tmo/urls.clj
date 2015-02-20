022b90e3d0dc3490d972bf34dfc77b298f7e4c00
Thomas Moerman <thomas.moerman@gmail.com>
2015-02-20 15:46:27 +0100

    blurb

--- repl-sessions/tmo/urls.clj
+++ repl-sessions/tmo/urls.clj
@@ -8,12 +8,13 @@
 (def oenanthotoxin "CCC[C@@H](O)CC\\C=C\\C=C\\C#CC#C\\C=C\\CO")
 (def three-ketocarbofuran "CNC(=O)Oc1cccc2C(=O)C(C)(C)Oc12")
 
-(defn gen-url
-  [smi]
-  (str "http://localhost:8000/molecule/" (url-encode smi)))
+(def big-one "C[C@@](C)(O1)C[C@@H](O)[C@@]1(O2)[C@@H](C)[C@@H]3CC=C4[C@]3(C2)C(=O)C[C@H]5[C@H]4CC[C@@H](C6)[C@]5(C)Cc(n7)c6nc(C[C@@]89(C))c7C[C@@H]8CC[C@@H]%10[C@@H]9C[C@@H](O)[C@@]%11(C)C%10=C[C@H](O%12)[C@]%11(O)[C@H](C)[C@]%12(O%13)[C@H](O)C[C@@]%13(C)CO")
+
+(defn gen-url [smi] (str "http://localhost:8000/molecule/" (url-encode smi)))
 
 (gen-url oenanthotoxin)
 (gen-url flavopereirin)
 (gen-url thiamine)
 (gen-url glucose)
+(gen-url big-one)
 
